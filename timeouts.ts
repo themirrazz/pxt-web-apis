64971b2f9432f62a3982d724bf1be777a49ce315
themirrazz <89490659+themirrazz@users.noreply.github.com>
2025-04-01 17:41:15 -0700

    Updated setTimeout, and now with setInterval support

--- timeouts.ts
+++ timeouts.ts
@@ -1,18 +1,45 @@
 // Add your code here
-let clearInterval = function (id: number) {
-    setTimeout(clearInterval, id);
-}
+let clearInterval = function (id: number) { }
 
-let setTimeout: any = (function () {
+let clearTimeout = function (id: number) { }
+
+let setInterval: any = (function () {
     let st_id = 1;
-    let cleared:number[] = [];
+    let cleared: number[] = [];
+    clearInterval = function (id: number) {
+        cleared.push(id);
+    }
     return function (
         func: Function,
         time?: number
     ) {
-        if(func === clearInterval) {
+        if (func === clearInterval) {
             return cleared.push(time);
         }
+        let t = time || 1;
+        let k = st_id;
+        st_id++;
+        let f = (function () {
+            basic.pause(t);
+            if (cleared.indexOf(st_id) < 0) {
+                func();
+                control.inBackground(f);
+            }
+        });
+        control.inBackground(f);
+    };
+})();
+
+let setTimeout: any = (function () {
+    let st_id = 1;
+    let cleared:number[] = [];
+    clearTimeout = function (id: number) {
+        cleared.push(id);
+    }
+    return function (
+        func: Function,
+        time?: number
+    ) {
         let t = time || 1;
         let k = st_id;
         st_id++;
